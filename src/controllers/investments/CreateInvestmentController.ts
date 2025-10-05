7106d4c51de1af2eacd7415fdabcaaed69022eb4
Pedro <pl2828280@gmail.com>
2025-10-05 16:23:26 -0300

    fix: ajusta on erro de await na criação de investimentos

--- src/controllers/investments/CreateInvestmentController.ts
+++ src/controllers/investments/CreateInvestmentController.ts
@@ -1,4 +1,4 @@
-import { CreateInvestmentService } from "../../services/investments/CreateInvestmentService";
+import { CreateInvestmentService } from "../../services/investment/CreateInvestmentService";
 import {Request, Response} from "express"
 
 class CreateInvestmentController{
@@ -6,7 +6,7 @@ class CreateInvestmentController{
     async handle (request:Request, response:Response){
         const {name,ticker,investedValue,quantity,user_id} = request.body;
         const createInvestmentService = new CreateInvestmentService();
-        const investment= createInvestmentService.execute({name,ticker,investedValue,quantity,user_id});
+        const investment= await createInvestmentService.execute({name,ticker,investedValue,quantity,user_id});
         return response.json(investment);
 
     }
