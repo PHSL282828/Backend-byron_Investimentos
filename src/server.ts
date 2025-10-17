99e9b847e292eb157a1665e68044927ee7961874
Pedro S.L <pl2828280@gmail.com>
2025-10-17 15:16:29 -0300

    feat: adiciona a documentação da api com swagger e atualiza o arquivo de rotas com rotas mais intuitivas

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import express, {Request, Response, NextFunction} from "express"
 import {router} from './routes'
+import cors from "cors"
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "../swagger.json"
+import path from "path"
 
 
 const app=express();
 const port=3333;
 
 app.use(express.json());
+app.use(cors())
+app.use("/v1/",router);//inicia o roteador pra enviar os dados entre api e local
+app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerDocument) )
+//inicializa o swagger para a documentação do codigo
 app.use(router)
+
 
 app.use((err:Error, request:Request, response:Response, next:NextFunction )=>{
     if (err instanceof Error){
@@ -18,6 +27,12 @@ app.use((err:Error, request:Request, response:Response, next:NextFunction )=>{
     return response.status(500).json({
         status:'error',
         message: 'Internal server error.'
+    })
+})
+
+app.get("/terms", (request:Request, response:Response)=>{
+    response.json({
+        message:"Termos de serviço"
     })
 })
 
